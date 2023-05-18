0232f7b9c6077fda305d16fb996a67f96a59a443
Franco Casas <casassfranco@gmail.com>
2023-05-18 16:47:28 -0400

    antes de state notifier

---
+++ lib/presentation/providers/theme_provider.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:widgets_app/config/theme/app_theme.dart';
+
+final isDarkModeProvider = StateProvider<bool>((ref) => false);
+
+//  Listado de colors inmutable (nunca va a cambiar)
+
+final colorListProvider = Provider((ref) => colorList);
+
+final selectedColorProvider = StateProvider<int>((ref) => 0);
